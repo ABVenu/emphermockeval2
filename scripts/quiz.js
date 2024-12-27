1be587d199d66455888df49db6851c06106debca
ABVenu <avenugopalb@gmail.com>
2024-12-27 16:53:50 +0530

    quiz CRUD completed

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,8 +1,8 @@
 import { baseUrl } from "./baseUrl.js";
 
-window.onload = ()=>{
-    getData()
-}
+window.onload = () => {
+  getData();
+};
 let form = document.getElementById("form");
 
 form.addEventListener("submit", async function () {
@@ -59,8 +59,8 @@ function getData() {
   fetch(`${baseUrl}/questions`)
     .then((res) => res.json())
     .then((data) => {
-     // console.log(data);
-      displayData(data)
+      // console.log(data);
+      displayData(data);
     })
     .catch((err) => {
       alert("something went wrong");
@@ -68,31 +68,96 @@ function getData() {
     });
 }
 
-function displayData(arr){
-    let cont = document.getElementById("cont")
-    cont.innerHTML = ""
-    arr.map((el,i)=>{
-        let card = document.createElement("div");
-        let title = document.createElement("h4");
-        title.textContent = `Title: ${el.title}`;
+function displayData(arr) {
+  let cont = document.getElementById("cont");
+  cont.innerHTML = "";
+  arr.map((el, i) => {
+    let card = document.createElement("div");
+    let title = document.createElement("h4");
+    title.textContent = `Title: ${el.title}`;
 
-        let optionA = document.createElement("h4");
-        optionA.textContent = `A: ${el.optionA}`;
+    let optionA = document.createElement("h4");
+    optionA.textContent = `A: ${el.optionA}`;
 
-        let optionB = document.createElement("h4");
-        optionB.textContent = `B: ${el.optionB}`;
+    let optionB = document.createElement("h4");
+    optionB.textContent = `B: ${el.optionB}`;
 
-        let optionC = document.createElement("h4");
-        optionC.textContent = `C: ${el.optionC}`;
+    let optionC = document.createElement("h4");
+    optionC.textContent = `C: ${el.optionC}`;
 
-        let optionD = document.createElement("h4");
-        optionD.textContent = `D: ${el.optionD}`;
+    let optionD = document.createElement("h4");
+    optionD.textContent = `D: ${el.optionD}`;
 
-        let correctOption = document.createElement("h4");
-        correctOption.textContent = `Correct Option: ${el.correctOption}`;
+    let correctOption = document.createElement("h4");
+    correctOption.textContent = `Correct Option: ${el.correctOption}`;
 
-        card.append(title,optionA,optionB,optionC,optionD,correctOption)
-        cont.append(card)
+    let reviewStatus = document.createElement("h4");
+    if (el.reviewStatus == true) {
+      reviewStatus.textContent = "Status: Reveiwed";
+    } else {
+      reviewStatus.textContent = "Status: Pending";
+    }
 
+    let reviewButton = document.createElement("button");
+    reviewButton.textContent = "Mark Review";
+    reviewButton.addEventListener("click", function () {
+      reviewFn(el);
+    });
+
+    let deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete Question";
+    deleteButton.addEventListener("click", function () {
+      deleteFn(el);
+    });
+    card.append(
+      title,
+      optionA,
+      optionB,
+      optionC,
+      optionD,
+      correctOption,
+      reviewStatus,
+      reviewButton,
+      deleteButton
+    );
+    cont.append(card);
+  });
+}
+
+function reviewFn(el) {
+  let updateQuestionObj = { ...el, reviewStatus: !el.reviewStatus};
+  //console.log(el)
+  // i need to change the review status of the question
+  // i will use PATCH Method
+  // https://sleepy-supreme-sleet.glitch.me/questions/4
+  fetch(`${baseUrl}/questions/${el.id}`, {
+    method: "PATCH",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(updateQuestionObj),
+  })
+    .then(() => {
+      alert("Question Updated Successfully");
+      getData()
     })
+    .catch((err) => {
+      alert("something went wrong");
+      console.log(err);
+    });
+}
+
+
+function deleteFn(el){
+    fetch(`${baseUrl}/questions/${el.id}`, {
+        method: "DELETE"
+      })
+        .then(() => {
+          alert("Question Deleted");
+          getData()
+        })
+        .catch((err) => {
+          alert("something went wrong");
+          console.log(err);
+        });
 }
